abf6de6cbfb83e19ddd9b553dadd557431e5eb98
yudong-yeob <maintain98@naver.com>
2024-04-27 11:51:08 +0900

    SecurityConfig 제거

--- src/main/java/GN/study/config/SecurityConfig.java
+++ src/main/java/GN/study/config/SecurityConfig.java
@@ -1,20 +1,20 @@
-package GN.study.config;
-
-
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig {
-
-    @Bean
-    @ConditionalOnProperty(name = "spring.h2.console.enabled", havingValue = "true")
-    public WebSecurityCustomizer webSecurityCustomizer(){
-        return web -> web.ignoring().requestMatchers(PathRequest.toH2Console());
-    }
-}
+//package GN.study.config;
+//
+//
+//import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+//import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig {
+//
+//    @Bean
+//    @ConditionalOnProperty(name = "spring.h2.console.enabled", havingValue = "true")
+//    public WebSecurityCustomizer webSecurityCustomizer(){
+//        return web -> web.ignoring().requestMatchers(PathRequest.toH2Console());
+//    }
+//}
