6ecf274cf78a99626b073dfa8e38ac2a3df8a9ef
dongyeobYu <maintain98@naver.com>
2024-04-29 11:01:28 +0900

    transaction

--- src/main/java/GN/study/user/service/UserService.java
+++ src/main/java/GN/study/user/service/UserService.java
@@ -4,20 +4,22 @@ import GN.study.user.dto.RequestUserDto;
 import GN.study.user.dto.ResponseUserDto;
 import GN.study.user.entity.User;
 import GN.study.user.repository.UserRepository;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class UserService {
 
     private final UserRepository userRepository;
 
 
+    @Transactional
     public ResponseUserDto createUser(RequestUserDto requestUserDto){
 
         User user = RequestUserDto.toEntity(requestUserDto);
@@ -25,6 +27,7 @@ public class UserService {
         return ResponseUserDto.toDto(userRepository.save(user));
     }
 
+    @Transactional
     public List<ResponseUserDto> findByAll(){
 
         return userRepository.findAll().stream().map(ResponseUserDto::toDto).collect(Collectors.toList());
