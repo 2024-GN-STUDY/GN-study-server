e31e916e19f9f26926b86bed5bcb8ff45c608d4a
dongyeobYu <maintain98@naver.com>
2024-05-02 17:23:43 +0900

    CORS 설정

--- src/main/java/GN/study/config/AppConfig.java
+++ src/main/java/GN/study/config/AppConfig.java
@@ -3,14 +3,27 @@ package GN.study.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class AppConfig {
+public class AppConfig implements WebMvcConfigurer {
 
     // 비밀번호 암호화 빈 등록
     @Bean
     BCryptPasswordEncoder bCryptPasswordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")                                        // CORS를 적용할 URL 패턴 설정
+                .allowedOrigins("http://localhost:3000", "http://localhost:8083")   // 허용할 URL 설정
+                .allowedMethods("GET", "POST", "PUT", "DELETE")                     // 허용할 Http Method 설정
+                .allowedHeaders("Authorization", "Content-Type")                    // 클라이언트 측의 CORS 요청에 허용되는 헤더 지정
+                .allowCredentials(true)                                             // True 설정 시 클라이언트 요청에 credentials 포함하고 응답으로 받기 가능
+                .maxAge(3600);
+
     }
 
 }
