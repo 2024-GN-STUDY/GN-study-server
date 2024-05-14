eb0040dc431e876e4980eee314a958be8e67cd37
suu1006 <1006pp@naver.com>
2024-05-14 22:33:03 +0900

    충돌 해결: 원격 저장소 내용으로 덮어쓰기

--- src/main/java/GN/study/user/controller/UserController.java
+++ src/main/java/GN/study/user/controller/UserController.java
@@ -1,54 +1,21 @@
 package GN.study.user.controller;
 
-import GN.study.user.dto.RequestUserSignUpDto;
-import GN.study.user.dto.ResponseUserSignUpDto;
-import GN.study.user.exception.UserExistException;
+import GN.study.user.dto.RequestUserDto;
+import GN.study.user.dto.ResponseUserDto;
 import GN.study.user.service.UserService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.ArraySchema;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.ErrorResponse;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import java.net.URI;
-import java.util.List;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/users")
 @RequiredArgsConstructor
-@Tag(name = "User-controller", description = "회원 서비스를 위한 컨트롤러")
 public class UserController {
 
     private final UserService userService;
 
-    @PostMapping
-    @Operation(summary = "회원가입 API")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "201", description = "회원가입 성공", content = @Content(schema = @Schema(implementation = ResponseUserSignUpDto.class))),
-            @ApiResponse(responseCode = "409", description = "이메일 중복", content = @Content()),
-    })
-    public ResponseEntity<ResponseUserSignUpDto> createUser(@Parameter(description = "회원가입 입력값", required = true, example = "RequestUserSignUpDto") @Valid @RequestBody RequestUserSignUpDto requestUserSignUpDto){
-
-            ResponseUserSignUpDto responseUserSignUpDto = userService.createUser(requestUserSignUpDto);
-
-            // hateoas 적용
-            URI location = ServletUriComponentsBuilder
-                    .fromCurrentContextPath()
-                    .path("/" + responseUserSignUpDto.getId())
-                    .buildAndExpand()
-                    .toUri();
-
-            return ResponseEntity.created(location).body(responseUserSignUpDto);
+    @PostMapping("/user")
+    public ResponseEntity<ResponseUserDto> createUser(RequestUserDto requestUserDto){
+        return ResponseEntity.ok(userService.createUser(requestUserDto));
     }
-
 }
--- src/main/java/GN/study/user/service/UserService.java
+++ src/main/java/GN/study/user/service/UserService.java
@@ -1,61 +1,21 @@
 package GN.study.user.service;
 
-import GN.study.user.dto.RequestUserSignUpDto;
-import GN.study.user.dto.ResponseUserSignUpDto;
-import GN.study.user.entity.Address;
-import GN.study.user.entity.Role;
+import GN.study.user.dto.RequestUserDto;
+import GN.study.user.dto.ResponseUserDto;
 import GN.study.user.entity.User;
-import GN.study.user.exception.UserExistException;
-import GN.study.user.mapper.UserMapper;
 import GN.study.user.repository.UserRepository;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
-@Transactional(readOnly = true)
 public class UserService {
 
     private final UserRepository userRepository;
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
-    private final UserMapper userMapper;
 
-    @Transactional
-    public ResponseUserSignUpDto createUser(RequestUserSignUpDto requestUserSignUpDto){
-
-        User validUser = userRepository.findByEmail(requestUserSignUpDto.getEmail());
-
-        // 중복 체크
-        if(validUser != null){
-            throw new UserExistException();
-        }
-
-        // 비밀번호 암호화
-        User user = User.builder()
-                .name(requestUserSignUpDto.getName())
-                .password(bCryptPasswordEncoder.encode(requestUserSignUpDto.getPassword()))
-                .email(requestUserSignUpDto.getEmail())
-                .role(Role.USER)
-                .age(requestUserSignUpDto.getAge())
-                .birth_dt(requestUserSignUpDto.getBirth_dt())
-                .phone_num(requestUserSignUpDto.getPhone_num())
-                .address(new Address(requestUserSignUpDto.getBaseAddr(), requestUserSignUpDto.getDetailAddr()))
-                .build();
-
-        //MapStruct 사용 requestUserDto -> User Entity 로 변경
-        return userMapper.toDto(userRepository.save(user));
-    }
-
-    @Transactional(readOnly = true)
-    public List<ResponseUserSignUpDto> findAll(){
-
-        return userRepository.findAll().stream()
-                .map(userMapper::toDto)
-                .collect(Collectors.toList());
+    public ResponseUserDto createUser(RequestUserDto requestUserDto){
+        User user = requestUserDto.toEntity(requestUserDto);
+        return ResponseUserDto.toDto(userRepository.save(user));
     }
 }
